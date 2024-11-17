f08ec2810fa3ea0b34fccf3b4b9b5b94492667fc
Kim Kudsk <kim.kudsk@gmail.com>
2024-11-18 00:45:05 +0100

    Created first table

--- tore.c
+++ tore.c
@@ -3,32 +3,48 @@
 #include "sqlite3.h"
 
 #define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
 #include "nob.h"
 
 
 int main(void)
 {
+  int result = 0;
+  // find home folder and then the database_path
   const char *home_path = getenv("HOME");
   if (home_path == NULL) {
     fprintf(stderr, "ERROR: No $HOME environment variable is setup. We need it to find the location of ~/.tore database.");
-    return 1;
+    return_defer(1);
   }
 
   const char *tore_path = nob_temp_sprintf("%s/.tore", home_path);
   printf("Database path: %s\n", tore_path);
 
+  // Open database
   sqlite3 *db;
-  sqlite3_stmt *stmt;
+  // sqlite3_stmt *stmt;
 
-  sqlite3_open(tore_path, &db);
-  if (db == NULL) {
-    fprintf(stderr, "ERROR: could not open %s\n", tore_path);
-    return 1;
+  int ret = sqlite3_open(tore_path, &db);
+  if (ret != SQLITE_OK) {
+    fprintf(stderr, "ERROR: %s, %s\n", tore_path, sqlite3_errstr(ret));
+    return_defer(1);
   }
-
   printf("Succesfull opened %s\n", tore_path);
 
-  sqlite3_close(db);
+  char *errmsg = NULL;
+  const char *sql = 
+    "CREATE TABLE IF NOT EXISTS Notifications (\n"
+    "   title VARCHAR(256) NOT NULL,\n"
+    "   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\n"
+    "   dismissed_at TIMESTAP DEFAULT NULL\n"
+    ")\n";
+  ret = sqlite3_exec(db, sql, NULL, NULL, &errmsg);
+  if (ret != SQLITE_OK) {
+    fprintf(stderr, "ERROR: could not create Notifications table: %s\n", errmsg);
+    return_defer(1);
+  }
 
-  return 0;
+defer:
+  if (db) sqlite3_close(db);
+  return result;
 }
