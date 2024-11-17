4055f57784217fac459cf521112deeeb80ecd633
Kim Kudsk <kim.kudsk@gmail.com>
2024-11-17 23:34:13 +0100

    getenv variable

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Tore 
 This is my version of Tsoding's tore project.
 
-'''batch
+```
 cc -o nob.c nob
 ./nob
-'''
+```
+
+```
--- tore.c
+++ tore.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "sqlite3.h"
 
 int main(void)
 {
-    printf("Hello World!\n");
+  const char *home_path = getenv("HOME");
+  if (home_path == NULL) {
+    fprintf(stderr, "ERROR: No $HOME environment variable is setup. We need it to find the location of ~/.tore database.");
+    return 1;
+  }
+
+    printf("HOME: is %s\n", home_path);
 
     return 0;
 }
